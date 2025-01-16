e574d6ef9380c8fdf89cc543fb2c43432b48f6aa
JussiTu <146333449+JussiTu@users.noreply.github.com>
2025-01-16 15:47:22 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 // Initialize Supabase
-const SUPABASE_URL = "https://bxqlpgmqchalrfmywofc.supabase.com"; // Replace with your Supabase URL
+const SUPABASE_URL = "https://bxqlpgmqchalrfmywofc.supabase.co"; // Replace with your Supabase URL
 const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJ4cWxwZ21xY2hhbHJmbXl3b2ZjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzcwMzcwMjIsImV4cCI6MjA1MjYxMzAyMn0.E6kWfJqepTSrsleKr5RSttS2OCFHRaT16JqC4HMEA38"; // Replace with your Supabase Anon Key
 const supabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
 
